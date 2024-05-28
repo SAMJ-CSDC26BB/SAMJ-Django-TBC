5c8a7a52eb9e1e4e196e995b37f54feb2dde8305
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2024-05-28 19:00:57 +0200

    Create first dummy views

--- samjTBC/samj/views.py
+++ samjTBC/samj/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the samj.")
+    return render(request, "index.html")
+
+def home(request):
+    return render(request, "home.html")

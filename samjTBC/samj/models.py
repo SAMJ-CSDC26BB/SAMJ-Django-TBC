bde57dd5abc434519b453576b48b1c06e967caa6
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2024-06-01 19:34:42 +0200

    Create models and register them in admin.py

--- samjTBC/samj/models.py
+++ samjTBC/samj/models.py
@@ -1,4 +1,5 @@
 import pytz
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 from django.db import models
@@ -24,3 +25,31 @@ class GlobalSettings(models.Model):
     language = models.CharField(max_length=2, choices=LANGUAGES, default='en')
     theme = models.CharField(max_length=5, choices=THEMES, default='light')
     notifications = models.CharField(max_length=3, choices=NOTIFICATIONS, default='on')
+
+
+class CallForwardingRecords(models.Model):
+    calledNumber = models.CharField(max_length=50, validators=[MinLengthValidator(1)], null=False, blank=False)
+    username = models.CharField(max_length=50, validators=[MinLengthValidator(1)], null=False, blank=False)
+    startDate = models.DateField(null=False, blank=False)
+    endDate = models.DateField(null=False, blank=False)
+
+
+class User(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('inactive', 'Inactive'),
+        ('deleted', 'Deleted'),
+    ]
+
+    ROLE_CHOICES = [
+        ('user', 'User'),
+        ('admin', 'Admin'),
+    ]
+
+    username = models.CharField(primary_key=True, max_length=50)
+    fullname = models.CharField(max_length=50, validators=[MinLengthValidator(1)], blank=False, null=False)
+    password = models.CharField(max_length=128, validators=[MinLengthValidator(1)], blank=False, null=False)
+    number = models.CharField(max_length=50, validators=[MinLengthValidator(1)], blank=False, null=False)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
+    role = models.CharField(max_length=5, choices=ROLE_CHOICES, default='user')
+    global_settings = models.OneToOneField(GlobalSettings, on_delete=models.CASCADE)

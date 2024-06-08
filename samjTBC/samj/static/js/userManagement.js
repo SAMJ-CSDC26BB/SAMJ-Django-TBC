ab0cab38f9298935a574b1cf178166ebf439b4c6
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2024-06-08 18:13:35 +0200

    Code refactoring

--- samjTBC/samj/static/js/userManagement.js
+++ samjTBC/samj/static/js/userManagement.js
@@ -2,60 +2,78 @@ import {initializeVanillaDataTable, showNotificationMessage} from './utils/utils
 
 document.addEventListener('DOMContentLoaded', function() {
     initializeVanillaDataTable('#userTable');
-    addEventListenersOnEditButtonClick();
-    addEventListenerOnDeleteButtonClick();
-    addEventListenersOnSaveButtonClick();
+    initializeEvents();
 });
 
-function addEventListenersOnEditButtonClick() {
+function initializeEvents() {
     document.querySelectorAll('.edit-user-btn').forEach(button => {
-        button.addEventListener('click', function(event) {
-            let row = getTableRowOfEditedUser(this),
-                form = document.querySelector('#userForm');
-
-            if (!form || !row) {
-                return;
-            }
-
-            let userNameInput = form.querySelector('#username');
-            userNameInput.value = row.dataset.username;
-            userNameInput.disabled = true;
-
-            form.querySelector('#fullname').value = row.dataset.fullname;
-            form.querySelector('#number').value = row.dataset.number;
-            form.querySelector('#status').value = row.dataset.status;
-            form.querySelector('#role').value = row.dataset.role;
-            form.querySelector('#password').value = '';
-        });
+        button.addEventListener('click', onEditButtonClick);
     });
+
+    document.querySelectorAll('.delete-user-btn').forEach(button => {
+        button.addEventListener('click', onDeleteButtonClick);
+    });
+
+    document.querySelector('#editCreateUserModal #saveUserBtn').addEventListener('click', onSaveButtonClick);
 }
 
-function addEventListenerOnDeleteButtonClick() {
-    document.querySelectorAll('.delete-user-btn').forEach(button => {
-        button.addEventListener('click', function(event) {
-            let row = getTableRowOfEditedUser(this),
-                deleteModalMessage = document.querySelector('.modal-body-message');
+function onEditButtonClick(event) {
+    let row = getTableRowOfEditedUser(this),
+        form = document.querySelector('#userForm');
 
-            if (!row) {
-                return;
-            }
+    if (!form || !row) {
+        return;
+    }
 
-            deleteModalMessage.innerText = deleteModalMessage.innerText.replace('{0}', row.dataset.username);
-        });
-    });
+    let userNameInput = form.querySelector('#username');
+    userNameInput.value = row.dataset.username;
+    userNameInput.disabled = true;
+
+    form.querySelector('#fullname').value = row.dataset.fullname;
+    form.querySelector('#number').value = row.dataset.number;
+    form.querySelector('#status').value = row.dataset.status;
+    form.querySelector('#role').value = row.dataset.role;
+    form.querySelector('#password').value = '';
+}
+
+function onDeleteButtonClick(event) {
+    let row = getTableRowOfEditedUser(this),
+        deleteModalMessage = document.querySelector('.modal-body-message');
+
+    if (!row) {
+        return;
+    }
+
+    deleteModalMessage.innerText = deleteModalMessage.innerText.replace('{0}', row.dataset.username);
 }
 
 function getTableRowOfEditedUser(context) {
     return context.closest('tr');
 }
 
-function addEventListenersOnSaveButtonClick() {
-    let saveButton = document.querySelector('#editCreateUserModal #saveUserBtn');
-    if (!saveButton) {
-        return;
-    }
+function onSaveButtonClick(event) {
+    const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 
-    saveButton.addEventListener('click', function() {
-        document.querySelector('#userForm').submit();
-    });
+    fetch('testAjax', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken
+        },
+        body: JSON.stringify({ username: "test", password: "test" })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            showNotificationMessage(data.message);
+        })
+        .catch(error => {
+            document.getElementById('responseMessage').innerText = 'Error creating user: ' + error;
+        });
+
+    //document.querySelector('#userForm').submit();
 }

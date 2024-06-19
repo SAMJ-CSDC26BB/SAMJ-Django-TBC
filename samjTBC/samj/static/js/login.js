fc42c3d5318b6b93a7b39e9794991ee94eeb9d44
Andreas Drozd <andreas.drozd@stud.fh-campuswien.ac.at>
2024-06-19 15:43:12 +0200

    apply patch

--- samjTBC/samj/static/js/login.js
+++ samjTBC/samj/static/js/login.js
@@ -1,33 +1,10 @@
 // Function to handle successful login
-function handleSuccessfulLogin() {
-    alert('Login successful! You can now use our service.');
-    // Redirect to the service page
-    window.location.href = '/service';
-}
+import * as Utils from './utils/utils.js';
 
-// Event listener for login button
-document.getElementById('login-button').addEventListener('click', function() {
-    // Perform login (this is just a placeholder - replace with your actual login code)
-    let loginSuccessful = true; // This should be the result of your login attempt
-
-    if (loginSuccessful) {
-        handleSuccessfulLogin();
-    } else {
-        alert('Login failed. Please try again.');
+document.addEventListener("DOMContentLoaded", function () {
+    let errormessage = document.querySelector('.login-error-message');
+    if (errormessage) {
+        Utils.showNotificationMessage(errormessage.innerText, "error");
+        errormessage.remove();
     }
-});
-
-$(document).ready(function() {
-    $("#show_hide_password a").on('click', function(event) {
-        event.preventDefault();
-        if($('#show_hide_password input').attr("type") === "text"){
-            $('#show_hide_password input').attr('type', 'password');
-            $('#show_hide_password i').addClass( "fa-eye-slash" );
-            $('#show_hide_password i').removeClass( "fa-eye" );
-        }else if($('#show_hide_password input').attr("type") === "password"){
-            $('#show_hide_password input').attr('type', 'text');
-            $('#show_hide_password i').removeClass( "fa-eye-slash" );
-            $('#show_hide_password i').addClass( "fa-eye" );
-        }
-    });
-});
+})

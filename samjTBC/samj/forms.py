a87917e5384208c3cb035417a2fec58af3741988
Andreas Drozd <andreas.drozd@stud.fh-campuswien.ac.at>
2024-06-24 17:33:35 +0200

    work on support page

--- samjTBC/samj/forms.py
+++ samjTBC/samj/forms.py
@@ -15,14 +15,28 @@ class GlobalSettingsForm(forms.ModelForm):
 
 
 class GitHubIssueForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    name = forms.CharField(max_length=100, required=False)
-    email = forms.EmailField(required=False)
-    telephone = forms.CharField(max_length=100, required=False)
-    url = forms.URLField(required=False)
-    short_description = forms.CharField(max_length=100)
+    title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'placeholder': '(optional) Enter the title here'
+    }))
+    name = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={
+        'placeholder': '(optional) Enter your name here'
+    }))
+    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs={
+        'placeholder': '(optional) Enter your email here'
+    }))
+    telephone = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={
+        'placeholder': '(optional) Enter your number here'
+    }))
+    url = forms.URLField(required=False, widget=forms.TextInput(attrs={
+        'placeholder': '(optional) Enter the URL where you encounter the error'
+    }))
+    short_description = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'placeholder': '(optional) Please summarize the issue in one sentence'
+    }))
     detailed_description = forms.CharField(widget=forms.Textarea)
-    steps_to_reproduce = forms.CharField(widget=forms.Textarea)
+    steps_to_reproduce = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': '1. \n2. \n3. \n...'
+    }))
     expected_results = forms.CharField(widget=forms.Textarea)
     actual_results = forms.CharField(widget=forms.Textarea)
 

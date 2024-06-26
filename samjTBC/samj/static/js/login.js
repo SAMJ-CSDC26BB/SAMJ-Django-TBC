75dde748fcf75d5175939129542623d5c3f34066
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2024-06-26 16:33:57 +0200

    Fix messages signup

--- samjTBC/samj/static/js/login.js
+++ samjTBC/samj/static/js/login.js
@@ -1,49 +1,31 @@
 // Function to handle successful authentication
 import * as Utils from './utils/utils.js';
+import {showMessagesOnDomLoad} from "./utils/utils.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-        let errormessage = document.querySelector('.authentication-error-message');
-        if (errormessage) {
-            Utils.showNotificationMessage(errormessage.innerText, "error");
-            errormessage.remove();
-        }
-    }
-)
-;
-
-$(document).ready(function () {
-    $('.toast').toast('show');
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    let errormessage = document.querySelector('.authentication-error-message');
-    if (errormessage) {
-        Utils.showNotificationMessage(errormessage.innerText, "error");
-        errormessage.remove();
-    }
+    Utils.showMessagesOnDomLoad('.loginMessage');
 
     // Get the input fields and the submit button
     let usernameInput = document.querySelector('#username_email_input');
     let passwordInput = document.querySelector('#password_input');
-    let submitButton = document.querySelector('#submit');
-
-    // Function to check if both fields are filled
-    function checkInputFields() {
-        if (usernameInput.value && passwordInput.value) {
-            submitButton.disabled = false;
-        } else {
-            submitButton.disabled = true;
-        }
-    }
-
-    // Check the input fields initially
-    checkInputFields();
 
     // Check the input fields whenever their values change
     usernameInput.addEventListener('input', checkInputFields);
     passwordInput.addEventListener('input', checkInputFields);
 });
 
-$(document).ready(function () {
-    $('.toast').toast('show');
-});
+function checkInputFields() {
+    let form = this.closest('form');
+    if (!form) {
+        return;
+    }
+
+    let usernameInput = form.querySelector('#username_email_input');
+    let passwordInput = form.querySelector('#password_input');
+    let submitButton = form.querySelector('[type="submit"]');
+    if (!usernameInput || !submitButton || !passwordInput) {
+        return;
+    }
+
+    submitButton.disabled = !(usernameInput.value && passwordInput.value);
+}
